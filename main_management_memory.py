a8795157abe055cb055be94bd138cd0ba6ec9e79
js3322 <js@cleancode.kr>
2025-04-07 15:54:22 +0900

    feat : mem profiler

--- main_management_memory.py
+++ main_management_memory.py
@@ -9,6 +9,8 @@ def myfunction(list_size):
     del mylist2
     return mylist
 
+myfunction(1000000)
+
 # mem_usage = memory_usage((myfunction, (), {'list_size': 1000000}))
 mem_usage = memory_usage((myfunction, (), {'list_size': 1000000}), max_usage=True)
 print(mem_usage)
