94f2e095e7526a767bab975adba4f7fe11e3d958
js3322 <js@cleancode.kr>
2025-04-07 15:53:59 +0900

    feat : mem profiler

---
+++ main_management_memory.py
@@ -0,0 +1,14 @@
+from memory_profiler import profile, memory_usage
+
+log_file = open('memory.log', 'w+')
+
+@profile
+def myfunction(list_size):
+    mylist = ['hello'] * list_size
+    mylist2 = ['world'] * list_size
+    del mylist2
+    return mylist
+
+# mem_usage = memory_usage((myfunction, (), {'list_size': 1000000}))
+mem_usage = memory_usage((myfunction, (), {'list_size': 1000000}), max_usage=True)
+print(mem_usage)
